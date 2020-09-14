2735d5deb545271e344830c25bc670d956356b5a
Domingo José Moronta <domingomoronta@gmail.com>
2020-09-14 13:50:40 -0700

    Added data science cookiecutter folder structure

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='The application of gradient boosting to  a regression problem, then to a classification problem using the Titanic dataset.',
+    author='Domingo Moronta',
+    license='MIT',
+)
